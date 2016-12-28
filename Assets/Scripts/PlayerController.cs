50204b9e3cc13b492e22592a1953ab9a00dd570d
JCGrant <jamescolin.grant@gmail.com>
2016-12-28 20:49:49 +0000

    Add thrust jump to Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,38 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    public float moveForce;
     public float maxSpeed;
+
+    public float moveForce;
     public float jumpForce;
+    public float thrustForce;
+
+    public int maxThrustPoints;
+
     public Transform groundCheck;
 
     private Rigidbody2D rb;
+
     private int groundLayerMask;
+
     private bool jumping;
     private bool grounded;
+    private bool thrusting;
+
+    private int thrustPoints;
+
+    private GameObject thrustFlame;
 
     void Start() {
         rb = GetComponent<Rigidbody2D>();
         groundLayerMask = 1 << LayerMask.NameToLayer("Ground");
         jumping = false;
         grounded = false;
+        thrustFlame = transform.Find("Thrust Flame").gameObject;
+        thrustPoints = maxThrustPoints;
+        InvokeRepeating("Regenerate", 1.0f, 1.0f);
     }
 
     void Update() {
@@ -28,6 +44,15 @@ public class PlayerController : MonoBehaviour {
 
         if (Input.GetButtonDown("Jump") && grounded) {
             jumping = true;
+        }
+
+        thrusting = Input.GetKey(KeyCode.LeftShift) && thrustPoints > 0;
+        thrustFlame.SetActive(thrusting);
+    }
+
+    private void Regenerate() {
+        if (thrustPoints < maxThrustPoints) {
+            thrustPoints += 1;
         }
     }
 
@@ -41,6 +66,11 @@ public class PlayerController : MonoBehaviour {
         if (jumping) {
             rb.AddForce(Vector2.up * jumpForce);
             jumping = false;
+        }
+
+        if (thrusting) {
+            rb.AddForce((Vector2.right + Vector2.up) * thrustForce);
+            thrustPoints -= 1;
         }
     }
 }
