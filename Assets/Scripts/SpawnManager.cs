0748af60286270df076b7b61e7ae1acfcc5c9eea
JCGrant <jamescolin.grant@gmail.com>
2016-12-28 16:44:26 +0000

    Spawn Player on a Ground platform

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+    public GameObject playerPrefab;
+    public Transform playerSpawn;
+
+    void Start() {
+        Instantiate(playerPrefab, playerSpawn.position, playerSpawn.rotation);
+    }
+}
