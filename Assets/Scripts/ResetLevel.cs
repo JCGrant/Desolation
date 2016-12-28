4a2c00de997b7115cf46499c7358f5b193995a1a
JCGrant <jamescolin.grant@gmail.com>
2016-12-28 17:47:17 +0000

    Reset Level when Player falls

---
+++ Assets/Scripts/ResetLevel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ResetLevel : MonoBehaviour {
+
+    void OnTriggerEnter2D (Collider2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
