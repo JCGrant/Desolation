e44ae58701ca1de18e9619ac87469404f3c2a038
JCGrant <jamescolin.grant@gmail.com>
2016-12-28 17:36:34 +0000

    Start a simple level with a falling platform

---
+++ Assets/Scripts/PlatformFall.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlatformFall : MonoBehaviour {
+
+    public float fallDelay;
+
+    private Rigidbody2D rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            Invoke("Fall", fallDelay);
+        }
+    }
+
+    void Fall() {
+        rb.isKinematic = false;
+    }
+}
